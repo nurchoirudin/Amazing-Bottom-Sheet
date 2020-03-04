4ff7e5d11eef169ddd97fd794117f9d1d77d3f58
Nurchoi <coy@clappingape.com>
2020-03-04 11:36:45 +0700

    Initial Commit

---
+++ Amazing-Bottom-Sheet/Amazing_Bottom_Sheet.h
@@ -0,0 +1,19 @@
+//
+//  Amazing_Bottom_Sheet.h
+//  Amazing-Bottom-Sheet
+//
+//  Created by Nur Choirudin on 04/03/20.
+//  Copyright © 2020 Nur Choirudin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Amazing_Bottom_Sheet.
+FOUNDATION_EXPORT double Amazing_Bottom_SheetVersionNumber;
+
+//! Project version string for Amazing_Bottom_Sheet.
+FOUNDATION_EXPORT const unsigned char Amazing_Bottom_SheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amazing_Bottom_Sheet/PublicHeader.h>
+
+
---
+++ Amazing-Bottom-SheetTests/Amazing_Bottom_SheetTests.swift
@@ -0,0 +1,34 @@
+//
+//  Amazing_Bottom_SheetTests.swift
+//  Amazing-Bottom-SheetTests
+//
+//  Created by Nur Choirudin on 04/03/20.
+//  Copyright © 2020 Nur Choirudin. All rights reserved.
+//
+
+import XCTest
+@testable import Amazing_Bottom_Sheet
+
+class Amazing_Bottom_SheetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
